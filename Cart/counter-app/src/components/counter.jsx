732cf6fddb2b8816dbb88e1ab34e1974b0e91061
coooopeeeer <miminashicooper@gmail.com>
2018-11-23 19:55:24 -0800

    Add title and button

--- Cart/counter-app/src/components/counter.jsx
+++ Cart/counter-app/src/components/counter.jsx
@@ -2,7 +2,13 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   render() {
-    return <h1>Hello World</h1>;
+    return (
+      // Use <React.Fragment> tag if I don't wanna use extra <div>
+      <React.Fragment>
+        <h1>Hello World</h1>
+        <button>Increment</button>
+      </React.Fragment>
+    );
   }
 }
 
